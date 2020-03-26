aaee7a94e98604da1f69006ea0e9b79bcc828277
Jeppevinkel <jeppevinkel@gmail.com>
2020-03-26 18:01:45 +0100

    Fixed some things, forgot to commit, and forgot what exactly I did

---
+++ GhostSpectator/Patches/PlayerEffectPatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets._Scripts.Dissonance;
+using Harmony;
+
+namespace GhostSpectator.Patches
+{
+	[HarmonyPatch(typeof(PlayerEffectsController))]
+	[HarmonyPatch(nameof(PlayerEffectsController.EnableEffect))]
+	[HarmonyPatch(new[]
+	{
+		typeof(string),
+	})]
+	class PlayerEnableEffectPatch
+	{
+		public static bool Prefix(PlayerEffectsController __instance, string apiName)
+		{
+			if (Plugin.GhostList.Contains(__instance.GetComponent<ReferenceHub>()))
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+	[HarmonyPatch(typeof(PlayerEffectsController))]
+	[HarmonyPatch(nameof(PlayerEffectsController.DisableEffect))]
+	[HarmonyPatch(new[]
+	{
+		typeof(string),
+	})]
+	class PlayerDisableEffectPatch
+	{
+		public static bool Prefix(PlayerEffectsController __instance, string apiName)
+		{
+			if (Plugin.GhostList.Contains(__instance.GetComponent<ReferenceHub>()))
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
